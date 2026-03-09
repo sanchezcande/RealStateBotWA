e4acef1368032da515e0f81c7ff2d45e78ae0ead
MSG1234 <msg1234@MSG1234s-MacBook-Pro.local>
2026-03-09 19:19:06 +0800

    fix22

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ DEEPSEEK_API_KEY = os.environ["DEEPSEEK_API_KEY"]
 DEEPSEEK_BASE_URL = "https://api.deepseek.com"
 DEEPSEEK_MODEL = "deepseek-chat"
 
-GOOGLE_SHEET_ID = os.environ.get("GOOGLE_SHEET_ID", "1EgEk6SXEf0LE3eKv7-jYm2L8K9gu5BiK")
+GOOGLE_SHEET_ID = os.environ.get("GOOGLE_SHEET_ID", "1mlysMKdK1vQp4zZBlsrl4AY28ZrNKCjCPRSHNY-qmgc")
 GOOGLE_CREDENTIALS_JSON = os.environ.get("GOOGLE_CREDENTIALS_JSON", "")
 
 SHEET_CACHE_TTL = int(os.environ.get("SHEET_CACHE_TTL", "300"))  # seconds
