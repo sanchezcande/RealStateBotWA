36461f76cc61359965cda144cb2164b8a4fef6e9
Candelaria Sanchez <sanchezgcandelaria@gmail.com>
2026-03-15 13:29:24 +0800

    Update DEEPSEEK_BASE_URL to use environment variable for flexibility

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN", "realstate_webhook_token")
 NOTIFY_NUMBER = os.environ["NOTIFY_NUMBER"]
 
 DEEPSEEK_API_KEY = os.environ["DEEPSEEK_API_KEY"]
-DEEPSEEK_BASE_URL = "https://api.deepseek.com"
+DEEPSEEK_BASE_URL = os.environ.get("DEEPSEEK_BASE_URL", "https://api.deepseek.com")
 DEEPSEEK_MODEL = "deepseek-chat"
 
 GOOGLE_SHEET_ID = os.environ.get("GOOGLE_SHEET_ID", "1mlysMKdK1vQp4zZBlsrl4AY28ZrNKCjCPRSHNY-qmgc")
